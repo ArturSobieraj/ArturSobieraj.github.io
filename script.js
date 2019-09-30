af82d99798ff757078c1f7c11b8a2409c2d5ce39
ArturSobieraj <artur.sobieraj126@gmail.com>
2019-09-30 18:32:33 +0200

    URL correction

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  const apiRoot = 'https://safe-shelf-52426.herokuapp.com/v1/task';
+  const apiRoot = 'https://safe-shelf-52426.herokuapp.com/v1/task/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const tasksContainer = $('[data-tasks-container]');
 
